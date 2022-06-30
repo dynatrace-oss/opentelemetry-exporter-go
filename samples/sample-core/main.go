7bc8efb3f11bb5816d5f45ed571f70414ff94c4f
Yurii Barninets <yurii.barninets@dynatrace.com>
2022-06-30 09:57:10 +0200

    Add sample-core app

---
+++ samples/sample-core/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"log"
+	"os"
+	"time"
+
+	"go.opentelemetry.io/otel"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	"go.opentelemetry.io/otel/trace"
+
+	"core/export"
+)
+
+func main() {
+	processor := export.NewDtSpanProcessor()
+	opt := sdktrace.WithSpanProcessor(processor)
+	otel.SetTracerProvider(sdktrace.NewTracerProvider(opt))
+
+	tracer := otel.Tracer("BasicOtelUseCase")
+
+	ctx, spanA := tracer.Start(context.Background(), "Span A")
+	endTimestamp := trace.WithTimestamp(time.Now().Add(750 * time.Millisecond))
+	spanA.End(endTimestamp)
+
+	ctx, spanB := tracer.Start(ctx, "Span B")
+	endTimestamp = trace.WithTimestamp(time.Now().Add(1250 * time.Millisecond))
+	spanB.End(endTimestamp)
+
+	err := processor.ForceFlush(ctx)
+	if err != nil {
+		log.Printf("Can not perform flush operation: %s", err)
+	}
+
+	ctx, spanC := tracer.Start(ctx, "Span C")
+	endTimestamp = trace.WithTimestamp(time.Now().Add(300 * time.Millisecond))
+	spanC.End(endTimestamp)
+
+	err = processor.Shutdown(ctx)
+	if err != nil {
+		log.Printf("Can not perform shutdown operation: %s", err)
+	}
+
+	// wait for user input before finish
+	_, _ = bufio.NewReader(os.Stdin).ReadBytes('\n')
+}
