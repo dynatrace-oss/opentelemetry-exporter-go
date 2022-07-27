6833ba8bdd5ab9df131dd492540ad6101f9a731f
kwinten.vandenberghe <kwinten.vandenberghe@dynatrace.com>
2022-07-27 13:05:13 +0200

    Added validation of SPRINT_VERSION

--- update_version.sh
+++ update_version.sh
@@ -1,3 +1,8 @@
+# Validate that $SPRING_VERSION conforms to the required pattern (e.g. 1.234.5), otherwise exit.
+echo $SPRINT_VERSION | grep -Pq '^\d+\.\d+\.\d+$' \
+    && echo "Sprint version is valid." \
+    || { echo "Sprint version does not match expected pattern."; exit 1; }
+
 # Replaces the Version constant in ./core/version/version.go with $SPRINT_VERSION
 cat > ./core/internal/version/version.go << EOM
 package version
