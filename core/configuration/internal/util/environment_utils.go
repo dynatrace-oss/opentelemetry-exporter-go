5e42fad9bf2371d3b64e673be6fb5bb5d02f8e0d
kwinten.vandenberghe <kwinten.vandenberghe@dynatrace.com>
2022-08-26 12:49:54 +0200

    Added copyright headers and linter rule to check for copyright in .go files

--- core/configuration/internal/util/environment_utils.go
+++ core/configuration/internal/util/environment_utils.go
@@ -1,3 +1,17 @@
+// Copyright 2022 Dynatrace LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package util
 
 import (
@@ -18,7 +32,7 @@ func GetIntFromEnvWithDefault(key string, defaultValue int) int {
 	if str, found := os.LookupEnv(key); found {
 		intVal, err := strconv.Atoi(str)
 		if err != nil {
-			fmt.Printf("Could not parse integer value from environment variable %s\n", key);
+			fmt.Printf("Could not parse integer value from environment variable %s\n", key)
 			return defaultValue
 		}
 		return intVal
@@ -30,7 +44,7 @@ func GetBoolFromEnvWithDefault(key string, defaultValue bool) bool {
 	if str, found := os.LookupEnv(key); found {
 		b, err := strconv.ParseBool(str)
 		if err != nil {
-			fmt.Printf("Could not parse boolean value from environment variable %s\n", key);
+			fmt.Printf("Could not parse boolean value from environment variable %s\n", key)
 			return defaultValue
 		}
 		return b
