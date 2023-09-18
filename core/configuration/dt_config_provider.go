8d8a09dc7e6d3395bc823f804434701217f3b6db
kwinten.vandenberghe <kwinten.vandenberghe@dynatrace.com>
2023-09-18 15:56:30 +0200

    [TEMP] extend flush timeout

--- core/configuration/dt_config_provider.go
+++ core/configuration/dt_config_provider.go
@@ -29,7 +29,7 @@ import (
 
 const (
 	DefaultFlushExportConnTimeoutMs   = 1000
-	DefaultFlushExportDataTimeoutMs   = 5000
+	DefaultFlushExportDataTimeoutMs   = 60_000
 	DefaultRegularExportConnTimeoutMs = 10000
 	DefaultRegularExportDataTimeoutMs = 60000
 )
