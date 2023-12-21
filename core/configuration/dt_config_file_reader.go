bf40949e52423c68b06e7af61f7b1fd05e2b788f
Kwinten Van den Berghe <kwinten.vandenberghe@dynatrace.com>
2023-12-21 11:35:04 +0100

    Improve reading config file (#38)
    * Fix config file reading for GCF
    
    * Allow specifying config path via env variable
    
    * Automatically fall back to other file paths if config file can't be found

--- core/configuration/dt_config_file_reader.go
+++ core/configuration/dt_config_file_reader.go
@@ -17,7 +17,9 @@ package configuration
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"io/ioutil"
+	"os"
 )
 
 type fileConfig struct {
@@ -55,10 +57,46 @@ type configFileReader interface {
 type jsonConfigFileReader struct {
 }
 
-// ReadConfigFromFile looks for a config file "dtconfig.json" in the current directory and attempts to parse it.
+// readConfigFromFile looks for a config file "dtconfig.json" and attempts to parse it.
 // Returns an error if the file can't be read or the parsing fails.
-func (j *jsonConfigFileReader) readConfigFromFile() (fileConfig, error) {
-	return j.readConfigFromFileByPath("./dtconfig.json")
+func (j *jsonConfigFileReader) readConfigFromFile() (cfg fileConfig, err error) {
+	filePaths := j.configFilePaths()
+
+	if len(filePaths) == 0 {
+		return fileConfig{}, errors.New("could not determine any file paths to read config file from")
+	}
+
+	for _, filePath := range filePaths {
+		cfg, err = j.readConfigFromFileByPath(filePath)
+		if err == nil {
+			return cfg, nil
+		}
+	}
+
+	return cfg, err
+}
+
+// configFilePaths returns all possible file paths to look for the config file in.
+func (j *jsonConfigFileReader) configFilePaths() []string {
+	var filePaths []string
+
+	if configFilePathFromEnv := os.Getenv("DT_CONFIG_FILE_PATH"); configFilePathFromEnv != "" {
+		filePaths = append(filePaths, configFilePathFromEnv)
+	}
+
+	// When running in a Google Cloud Functions Go runtime, we need to find the config file at a different path.
+	// For reference on the K_SERVICE environment variable, see:
+	// https://cloud.google.com/functions/docs/configuring/env-var#runtime_environment_variables_set_automatically
+	_, inGcf := os.LookupEnv("K_SERVICE")
+	if inGcf {
+		// In the GCF Go runtime, the root directory of your function source code is
+		// beneath the current working directory at ./serverless_function_source_code
+		// See https://cloud.google.com/functions/docs/concepts/execution-environment#memory-file-system
+		filePaths = append(filePaths, "./serverless_function_source_code/dtconfig.json")
+	}
+
+	filePaths = append(filePaths, "./dtconfig.json")
+	return filePaths
 }
 
 func (j *jsonConfigFileReader) readConfigFromFileByPath(filePath string) (fileConfig, error) {
